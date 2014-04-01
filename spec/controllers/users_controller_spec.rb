990c6a3fb3676480b1270875e0a8b0a082e5e5b7
Whitney Levis <ms.whitney.levis@gmail.com>
2014-04-01 11:46:25 -0700

    User Controller rspec structure

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,27 +1,95 @@
 require 'spec_helper'
 
 describe UsersController do
-  describe "new" do
-    
+  describe "GET new" do
+    it "displays the new template" do
+      
+    end
   end
 
-  describe "create" do
-    
+  describe "POST create" do
+    context "with valid attributes" do
+      it "saves the user to the database" do
+        
+      end
+
+      it "redirects the user" do
+        
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the user to the database" do
+        
+      end
+
+      it "re-renders the new page" do
+        
+      end
+
+      it "displays an error message for the user" do
+        
+      end
+    end
   end
 
-  describe "edit" do
-    
+  describe "GET edit" do
+    it "locates the requested User" do
+      
+    end
+
+    it "renders the edit page" do
+      
+    end
   end
 
-  describe "update" do
-    
+  describe "PATCH update" do
+    context "with valid attributes" do
+      it "locates the correct user" do
+        
+      end
+
+      it "updates the user to the database" do
+        
+      end
+
+      it "redirects the user to the user's show pages" do
+        
+      end
+    end
+
+    context "with invalid attributes" do
+      it "displays an error message for the user" do
+        
+      end
+
+      it "does not update the user in the database" do
+        
+      end
+
+      it "re-renders the edit page" do
+        
+      end
+    end
   end
 
-  describe "show" do
-    
+  describe "GET show" do
+    it "locates the requested User" do
+      
+    end
+
+    it "renders the show page" do
+      
+    end
   end
 
-  describe "index" do
-    
+  describe "GET index" do
+    it "locates the users and populates a user array" do
+      
+    end
+
+    it "renders the index page" do
+      
+    end
   end
 end
