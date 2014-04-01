8be9ad2003203e1a9771284dce2c81b6f5e59df5
Whitney Levis <ms.whitney.levis@gmail.com>
2014-04-01 10:58:33 -0700

    Initial User controller generator

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe UsersController do
+  describe "new" do
+    
+  end
+
+  describe "create" do
+    
+  end
+
+  describe "edit" do
+    
+  end
+
+  describe "update" do
+    
+  end
+
+  describe "show" do
+    
+  end
+
+  describe "index" do
+    
+  end
+end
