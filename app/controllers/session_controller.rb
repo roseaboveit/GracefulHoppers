2d0b88ed60beee7bcf5d6c4afb4fc138823541c5
Whitney Levis <ms.whitney.levis@gmail.com>
2014-04-09 14:39:39 -0700

    Logic for redirecting user to user_edit page if user has example@example.com email address

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,11 @@ class SessionController < ApplicationController
     user = User.from_omniauth(auth_hash)
     session[:user_id] = user.id
 
-    redirect_to root_path, notice: "Signed In!"
+    if user.email = 'example@example.com'
+      redirect_to edit_user_path(user.id), notice: "Twitter Authenticated! Please fill out your profile."
+    else
+      redirect_to root_path, notice: "Signed In!"
+    end
   end
 
   def destroy
