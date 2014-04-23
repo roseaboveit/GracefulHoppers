8b01fd1c80155510bf653599dc8a71f0f920dd83
Whitney Levis <ms.whitney.levis@gmail.com>
2014-04-23 13:56:35 -0700

    User's unit starts at 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user.unit == 0
+      @user.unit = 1
+      @user.save
+    end
   end
 
   def index
